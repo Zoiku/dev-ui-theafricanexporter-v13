7e92c4871cb8e03fe683a5949a789ecd93de18e7
Mark Zoiku <markkwesizoiku@gmail.com>
2023-05-29 11:41:35 +0000

    updateMobileButtonsFix

--- src/Services/AxiosInstance.js
+++ src/Services/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = () => {
-  const url = process.env.REACT_APP_BASE_URL_PRODUCTION;
+  const url = process.env.REACT_APP_BASE_URL_BASE_URL_DEVELOPMENT;
 
   let headers = {
     "Content-Type": "application/json",
