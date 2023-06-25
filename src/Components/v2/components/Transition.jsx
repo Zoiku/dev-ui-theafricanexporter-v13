d60879d211e06ea411f8e4d09179636d4cc745a6
Mark Zoiku <markkwesizoiku@gmail.com>
2023-06-25 20:17:16 +0000

    latest-update

--- src/Components/v2/components/Transition.jsx
+++ src/Components/v2/components/Transition.jsx
@@ -1,10 +1,7 @@
-import { Slide, SlideProps } from "@mui/material/";
+import { Slide } from "@mui/material/";
 import { forwardRef } from "react";
 
-export const Transition = forwardRef(function Transition(
-  props: SlideProps,
-  ref
-) {
+export const Transition = forwardRef(function Transition(props, ref) {
   return (
     <Slide mountOnEnter unmountOnExit direction="down" ref={ref} {...props} />
   );
